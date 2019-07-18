0275d1774efba9789b57aa99b83af3564ce5bbea
Chris Choyce <christopher.choyce@gmail.com>
2019-07-18 00:25:07 -0400

    Refactor to allow client to be embedded in the server

--- server/app.js
+++ server/app.js
@@ -11,12 +11,13 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/', indexRouter);
 app.use('/reservations', usersRouter);
